a17602cb98c140bfd5ff486c9ed977516832d912
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-07-02 17:26:27 -0400

    refactor: enhance LandingSearch component layout and add search icon

--- app/(components)/landingSearch.tsx
+++ app/(components)/landingSearch.tsx
@@ -1,14 +1,16 @@
+import { Search } from 'lucide-react';
+
 export default function LandingSearch() {
     return (
-        <div className="backdrop-blur-md bg-zinc-900/60 border border-green-900 rounded-2xl shadow-xl px-3 py-2 flex flex-col md:flex-row gap-4 items-center w-[35%] mr-auto">
+        <div className="md:backdrop-blur-md md:bg-zinc-900/60 md:border md:border-green-900 rounded-2xl md:shadow-xl px-3 py-2 flex flex-col md:flex-row gap-4 items-center w-[35%] mr-auto">
             <input
                 type="text"
                 placeholder="Search..."
-                className="flex-1 px-4 py-2 bg-zinc-800/70 text-white placeholder:text-zinc-400 font-[EBGaramond] text-lg tracking-wide rounded-md focus:outline-none focus:ring-2 focus:ring-green-700 transition"
+                className="hidden md:block flex-1 px-4 py-2 bg-zinc-800/70 text-white placeholder:text-zinc-400 font-[EBGaramond] text-lg tracking-wide rounded-md focus:outline-none focus:ring-2 focus:ring-green-700 transition"
             />
 
             <select
-                className="px-4 py-1 bg-zinc-800/70 text-white font-[EBGaramond] text-lg rounded-md focus:outline-none focus:ring-2 focus:ring-green-700"
+                className="hidden md:block px-4 py-1 bg-zinc-800/70 text-white font-[EBGaramond] text-lg rounded-md focus:outline-none focus:ring-2 focus:ring-green-700"
             >
                 <option value="everywhere">Everywhere</option>
                 <option value="statusUpdates">Status Updates</option>
@@ -23,7 +25,7 @@ export default function LandingSearch() {
             <button
                 className="px-6 py-2 bg-green-800 hover:bg-green-700 text-white font-[Amendra] text-xl rounded-md transition tracking-wider shadow-md"
             >
-                Search
+                <Search />
             </button>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import StandardSection from './(components)/standardSection';
 export default function Home() {
   return (
     <div className="w-screen overflow-x-hidden">
-      <section className="h-[80vh] flex flex-col items-center justify-center text-white">
-        <h1 className="font-[Almendra] text-[10em] text-transparent bg-clip-text bg-gradient-to-r from-[#4C6B4F] via-[#5E7F57] to-[#3E5942] drop-shadow-[0_2px_4px_rgba(0,0,0,0.7)]">
+      <section className="md:h-[80vh] flex flex-col items-center justify-center text-white mb-8 md:mb-0">
+        <h1 className="font-[Almendra] text-[5em] text-center md:text-[10em] text-transparent bg-clip-text bg-gradient-to-r from-[#4C6B4F] via-[#5E7F57] to-[#3E5942] drop-shadow-[0_2px_4px_rgba(0,0,0,0.7)]">
           Vintage Story
         </h1>
-        <p className="flex justify-center w-[65%] text-2xl font-[EBGaramond] mt-12">Vintage Story is an uncompromising wilderness survival sandbox game inspired by eldritch horror themes. Find yourself in a ruined world reclaimed by nature and permeated by unnerving temporal disturbances. Relive the advent of human civilization, or take your own path.</p>
+        <p className="flex justify-center w-[90%] md:w-[65%] text-2xl font-[EBGaramond] mt-12">Vintage Story is an uncompromising wilderness survival sandbox game inspired by eldritch horror themes. Find yourself in a ruined world reclaimed by nature and permeated by unnerving temporal disturbances. Relive the advent of human civilization, or take your own path.</p>
       </section>
       <section className="bg-zinc-900/80 backdrop-blur-md px-6 py-12 rounded-2xl w-[90%] max-w-6xl mx-auto text-white shadow-xl border border-zinc-700">
         <div className="flex flex-col md:flex-row justify-between items-center gap-10">
